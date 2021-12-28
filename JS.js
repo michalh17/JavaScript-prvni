f4a22bbca1c5cdc266c8a4fbf0277e054f5de66a
Michal Hrtoň <45671548+michalh17@users.noreply.github.com>
2021-12-28 19:38:26 +0100

    Zkouska

--- JS.js
+++ JS.js
@@ -1,1 +1,14 @@
+<html>
+    <body>
+        <canvas width="600" height="600">
 
+        </canvas>
+
+        <script>
+            let year=2042;
+            let nextYear=year+1;
+
+            let dinner='pasta';
+        </script>
+    </body>
+</html>
